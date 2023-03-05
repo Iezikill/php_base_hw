c781c9ab63041d2b15e374ef0f25b72de68b89a3
Viktoriya <viktoriya.shiryaeva54@yandex.ru>
2023-03-05 10:47:05 +0200

    hw5 is completed

--- lesson5/Task.php
+++ lesson5/Task.php
@@ -1,82 +1,86 @@
 <?php
-// Задание 1. 
-// Разработайте класс Task, выполняющий ответственность обычной задачи 
-// Todo-списка. Класс должен содержать приватные свойства description, 
-// dateCreated, dateUpdated, dateDone, priority (int), isDone (bool) и 
-// обязательное user (User). В качества класса пользователя воспользуйтесь 
-// разработанным на уроке. Класс Task должен содержать все необходимые для 
-// взаимодействия со свойствами методы (getters, setters). При вызове метода 
-// setDescription обновляйте значение свойства dateUpdated. Разработайте 
-// метод markAsDone, помечающий задачу выполненной, а также обновляющий 
-// свойства dateUpdated и dateDone.
+
 require_once 'User.php';
 require_once 'Comment.php';
 
 class Task
 {
-  private string $description;
-  private DateTime $dateCreated;
-  private DateTime $dateUpdated;
-  private DateTime $dateDone;
-  private int $priority;
-  private bool $isDone = false;
   private User $user;
-  private array $comments = [];
+  private int $priority;
+  private string $description;
 
-  public function __construct(User $user)
+  private string $dateCreated;
+
+  private string $dateUpdated;
+
+  private bool $isDone = false; //по умолчанию задача не выполнена
+  private string $dateDone;
+
+  private array $comments; //здесь хранится массив комментариев
+
+  public function __construct(User $user, int $priority, string $description)
   {
     $this->user = $user;
-    $this->dateCreated = new DateTime();
+    $this->priority = $priority;
+    $this->description = $description;
+    $this->dateCreated = $this->dateUpdated = date('Y-m-d H:i:s');
   }
 
-  public function getDateCreated(): DateTime
+  public function setDescription(string $description): void
   {
-    return $this->dateCreated;
+    $this->description = $description;
+    $this->dateUpdated = date('Y-m-d H:i:s');
   }
-  public function setDateCreated(DateTime $dateCreated): void
-  {
-    $this->dateCreated = $dateCreated;
-  }
-  public function getDateUpdated(): DateTime
-  {
-    return $this->dateUpdated;
-  }
-  public function setDateUpdated(DateTime $dateUpdated): void
-  {
-    $this->dateUpdated = $dateUpdated;
-  }
+
   public function getDescription(): string
   {
     return $this->description;
   }
-  public function setDescription(string $description): void
-  {
-    $this->description = $description;
-    $this->setDateUpdated(new DateTime());
-  }
-  public function getPriority(): int
-  {
-    return $this->priority;
-  }
-  public function setPriority(int $priority): void
-  {
-    $this->priority = $priority;
-  }
+
   public function markAsDone(): void
   {
-    $this->dateDone = new DateTime();
+    $this->dateDone = $this->dateUpdated = date('Y-m-d H:i:s');
     $this->isDone = true;
   }
-  public function getUser(): User
-  {
-    return $this->user;
-  }
-  public function setUser(User $user): void
-  {
-    $this->user = $user;
-  }
-  public function addComment(Comment $comment)
+
+  public function addComment(Comment $comment): void
   {
     $this->comments[] = $comment;
   }
+
+  // public function getDateCreated(): DateTime
+  // {
+  //   return $this->dateCreated;
+  // }
+  // public function setDateCreated(DateTime $dateCreated): void
+  // {
+  //   $this->dateCreated = $dateCreated;
+  // }
+  // public function getDateUpdated(): DateTime
+  // {
+  //   return $this->dateUpdated;
+  // }
+  // public function setDateUpdated(DateTime $dateUpdated): void
+  // {
+  //   $this->dateUpdated = $dateUpdated;
+  // }
+
+  // public function getPriority(): int
+  // {
+  //   return $this->priority;
+  // }
+  // public function setPriority(int $priority): void
+  // {
+  //   $this->priority = $priority;
+  // }
+
+  // public function getUser(): User
+  // {
+  //   return $this->user;
+  // }
+  // public function setUser(User $user): void
+  // {
+  //   $this->user = $user;
+  // }
+
 }
--- lesson5/User.php
+++ lesson5/User.php
@@ -4,38 +4,12 @@ class User
 {
   private string $username;
   private string $email;
-  private ?string $sex;
   private ?int $age;
-  private bool $isActive = true;
-  private DateTime $dateCreated;
 
-  function __construct(string $username, string $email)
+
+  function __construct(string $username, string $email) //конструткор
   {
     $this->username = $username;
     $this->email = $email;
-    $this->dateCreated = new DateTime();
-  }
-
-  function getUsername(): string
-  {
-    return $this->username ?? 'unknown';
-  }
-
-  private function getValidAge(?int $age): ?int
-  {
-    if ($age > 0 && $age <= 125) {
-      return $age;
-    }
-    return null;
-  }
-
-  public function setAge(?int $age): void
-  {
-    $this->age = $this->getValidAge($age);
-  }
-
-  function getAge(): ?int
-  {
-    return $this->age;
   }
 }
